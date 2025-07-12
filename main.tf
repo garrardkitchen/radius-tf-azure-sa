626250eb9a7de7d1e3f926ec5765a43bc60ce4f7
Garrard <garrard.kitchen@fujitsu.com>
2025-07-12 13:26:18 +0100

    fix: Corrected reference to storage_account_name variable in storage account resource

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_resource_group" "main" {
 
 # Create a storage account
 resource "azurerm_storage_account" "main" {
-  name                     = storage_account_name
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
